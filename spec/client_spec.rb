590e85504e9cddfdc9232aa9ae45d6295fc0329b
Your Name <majek04@gmail.com>
2010-11-09 14:10:36 +0000

    Added more complex wildcard test.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -46,7 +46,7 @@ describe RATS do
     EM::run do
       rats = RATS.new
       rats.subscribe('exit') { rats.stop {EM.stop} }
-      s = rats.subscribe('#') { |msg| msgs << msg }
+      rats.subscribe('#') { |msg| msgs << msg }
       rats.publish('a', 'a')
       rats.publish('b', 'b')
       rats.publish('exit')
@@ -58,7 +58,7 @@ describe RATS do
     msgs = []
     EM::run do
       rats = RATS.new
-      s = rats.subscribe('a') do |msg, reply_to|
+      rats.subscribe('a') do |msg, reply_to|
         rats.reply(reply_to, msg + '_r')
       end
       rats.request('a', 'a') { |msg| msgs << msg }
@@ -68,16 +68,15 @@ describe RATS do
     msgs.should == ['a_r', 'b_r']
   end
 
-
   it 'should be able to handle two connections in one EM block' do
     msgs = []
     EM::run do
       rats = RATS.new
       rats2 = RATS.new nil, :exchange => 'rats2'
-      s = rats.subscribe('a') do |msg, reply_to|
+      rats.subscribe('a') do |msg, reply_to|
         rats.reply(reply_to, msg + '_1')
       end
-      s = rats2.subscribe('a') do |msg, reply_to|
+      rats2.subscribe('a') do |msg, reply_to|
         rats2.reply(reply_to, msg + '_2')
       end
       rats.request('a', '1') { |msg| msgs << msg }
@@ -89,6 +88,22 @@ describe RATS do
     msgs.sort.should == ['1_1', '1_1', '2_2', '2_2']
   end
 
+  it 'should handle complex wildcard subscriptions' do
+    msgs = []
+    EM::run do
+      rats = RATS.new
+      rats.subscribe('exit') { rats.stop {EM.stop} }
+      rats.subscribe('a.*') { |msg,_, topic| msgs << topic }
+      rats.subscribe('a.*.a') { |msg,_,topic| msgs << topic }
+      rats.subscribe('a.a.#') { |msg,_,topic| msgs << topic }
+      rats.publish('a')
+      rats.publish('a.a')
+      rats.publish('a.a.a')
+      rats.publish('a.a.a.a')
+      rats.publish('exit')
+    end
+    msgs.sort.should == ['a.a', 'a.a', 'a.a.a', 'a.a.a', 'a.a.a.a']
+  end
 end
 
 
